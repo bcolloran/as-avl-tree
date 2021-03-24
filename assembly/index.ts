6b1d4ff8be5880615e320e1e704fc0ea00474c2f
bcolloran <brendan@colloran.net>
2021-03-24 16:10:38 -0700

    add index, fix package name

---
+++ assembly/index.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/// <reference path="../node_modules/assemblyscript/std/assembly/rt/index.d.ts" />
+// import { expect, Expectation } from "./internal/Expectation";
+// import {
+//   afterAll,
+//   afterEach,
+//   beforeAll,
+//   beforeEach,
+//   debug,
+//   describe,
+//   it,
+//   itThrows,
+//   test,
+//   throws,
+//   todo,
+//   xit,
+//   xtest,
+// } from "./internal/Test";
+// import { log } from "./internal/log";
+// import { Reflect } from "./internal/Reflect";
+// import { Expected } from "./internal/Expected";
+// export { __call } from "./internal/call";
+// export { __ignoreLogs } from "./internal/log";
+
+import { AvlTree } from "./avlTree/avlTree";
+import { AvlTreeMap } from "./avlTreeMap/avlTreeMap";
